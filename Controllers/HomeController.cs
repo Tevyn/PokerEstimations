3f20ec3badffac332c244225c0b7f291437b3b2a
Tevyn <tevynbell@gmail.com>
2021-08-29 13:34:28 -0700

    Removing unused methods

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,32 +27,6 @@ namespace PokerEstimations.Controllers
             return View(model);
         }
 
-        [HttpPost]
-        public IActionResult Guess(string[] pocketCards, double guess)
-        {
-            var calculateEquitySerice = new CalculateEquityService();
-            var result = calculateEquitySerice.CalculateEquity(pocketCards)[0];
-            var diff = result - guess;
-            bool correct = diff < 5 && diff > -5;
-
-
-            return RedirectToAction("CheckAnswer", correct);
-        }
-
-        public IActionResult CheckAnswer(bool correct)
-        {
-            if (correct)
-            {
-                ViewBag.CheckAnswerMessage = "Close enough";
-            }
-            else
-            {
-                ViewBag.CheckAnswerMessage = "Wrong";
-            }
-            return View();
-        }
-
-
         public IActionResult Privacy()
         {
             return View();
